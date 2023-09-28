3919f236aba311b693818b33cc7bc25ed4b4bc1c
DMGamage <gamagemadhushan@gmail.com>
2023-09-28 13:18:26 +0530

    first commit

--- Documents/Spring Microservices/Project/ProductService/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ Documents/Spring Microservices/Project/ProductService/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -2,16 +2,20 @@ package com.example.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.google.common.net.MediaType;
 
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
     @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
@@ -23,7 +27,8 @@ class ProductServiceApplicationTests {
     }
 
     @Test
-    void shouldCreateAProduct() {
+    void shouldCreateAProduct() throws Exception {
+
     }
 
 }
